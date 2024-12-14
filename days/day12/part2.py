9d39a5724679830f61e44134c30fd9046e82cbd5
Ben Berry <bengerman@gmail.com>
2024-12-14 15:24:47 -0800

    day12 part2 solved

--- days/day12/part2.py
+++ days/day12/part2.py
@@ -1,6 +1,13 @@
 import sys
 from dataclasses import dataclass, field
 from typing import Optional
+import itertools
+
+
+@dataclass(order=True, frozen=True)
+class Heading:
+    x: int
+    y: int
 
 
 @dataclass(order=True, frozen=True)
@@ -27,6 +34,22 @@ class Coord:
     @property
     def neighbors(self) -> tuple["Coord", "Coord", "Coord", "Coord"]:
         return self.north, self.south, self.east, self.west
+
+    def __sub__(self, other):
+        x = self.x - other.x
+        y = self.y - other.y
+        return Heading(x=x, y=y)
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Coord(x=x, y=y)
+
+
+@dataclass(frozen=True, order=True)
+class Vector:
+    origin: Coord
+    heading: Heading
 
 
 @dataclass(frozen=True)
@@ -62,11 +85,78 @@ class Region:
 
     @property
     def perimeter(self):
-        return sum([plot.fences for plot in self.plots])
+        return sum([plot.num_fences for plot in self.plots])
+
+    def perimeter_plots(self):
+        return [plot for plot in self.plots if plot.num_fences > 0]
+
+    def count_sides(self):
+        def process_fences_by_heading(fences_: list[Vector], heading: Heading) -> int:
+            """
+            find the number of sides facing a given direction
+            """
+
+            sides = 0
+            if heading.x:
+                filter_function = lambda x: x.origin.x
+            elif heading.y:
+                filter_function = lambda x: x.origin.y
+
+            fences_.sort(key=filter_function)
+
+            for row_column, fence_group in itertools.groupby(fences_, filter_function):
+                group = list(fence_group)
+                print(f"{row_column=}, {heading=}, {group=}")
+                sides += process_fences_by_row_column(fences_=group, heading=heading)
+            return sides
+
+        def process_fences_by_row_column(
+            fences_: list[Vector], heading: Heading
+        ) -> int:
+            """
+            find the number of sides facing a given direction and perpendicular line
+            """
+            if heading.x:
+                pos = lambda x: x.origin.y
+            elif heading.y:
+                pos = lambda x: x.origin.x
+            fences_.sort(key=pos)
+            last_pos = -1
+            run_starts = list()
+            for fence in fences_:
+                print(f"\t\t{fence=}")
+                this_pos = pos(fence)
+                if last_pos == -1:
+                    # prime the loop
+                    last_pos = this_pos
+                    run_starts.append(last_pos)
+                    continue
+                if abs(this_pos - last_pos) != 1:
+                    run_starts.append(this_pos)
+                last_pos = this_pos
+            print(f"\t{run_starts=}")
+            return len(run_starts)
+
+        fences = []
+        direction = None
+        for plot in self.perimeter_plots():
+            for fence in plot.fences:
+                fences.append(fence)
+        fences = sorted(fences, key=lambda x: x.heading)
+        sides = 0
+        for heading, group in itertools.groupby(fences, lambda x: x.heading):
+            fence_group = list(group)
+            sides += process_fences_by_heading(fence_group, heading)
+        print(f"{sides=}")
+        return sides
 
     @property
     def fence_cost(self) -> int:
         return self.area * self.perimeter
+
+    @property
+    def fence_cost_discount(self) -> int:
+        return self.area * self.count_sides()
 
     def __contains__(self, plot: "Plot"):
         return plot in self.plots
@@ -99,12 +189,32 @@ class Plot:
         ]
 
     @property
-    def fences(self):
+    def region_neighbors(self):
+        return [n for n in self.neighbors if n.plant == self.plant]
+
+    @property
+    def non_region_neighbors(self):
+        return [
+            n
+            for n in self.coord.neighbors
+            if n not in self.grid or self.grid[n].plant != self.plant
+        ]
+
+    @property
+    def num_fences(self):
         fences = 4
         for neighbor in self.neighbors:
             # print(f"{self=}, {neighbor=}")
             if neighbor in self.region:
                 fences -= 1
+        return fences
+
+    @property
+    def fences(self):
+        fences = []
+        for neighbor in self.non_region_neighbors:
+            heading = neighbor - self.coord
+            fences.append(Vector(self.coord, heading))
         return fences
 
 
@@ -147,12 +257,17 @@ def main():
                 farm.regions[region.plant] = farm.regions.get(region.plant, [])
                 farm.regions[region.plant].append(region)
 
-    total = 0
+    total_old = 0
+    total_discount = 0
     for plant, regions in farm.regions.items():
         for region in regions:
             region.print(farm)
-            total += region.fence_cost
-    print(total)
+            discount_cost = region.fence_cost_discount
+            # print(f"{region.fence_cost=}")
+            print(f"{discount_cost=}")
+            total_old += region.fence_cost
+            total_discount += discount_cost
+    print(f"{total_old=}, {total_discount=}")
 
 
 if __name__ == "__main__":
